805540ba629f481baa7c35bedcdb098ae95570a7
wubin <wubin@admaster.com.cn>
2013-05-28 22:26:05 +0800

    modify the user/models.py. add comments, fix the functions.

--- user/models.py
+++ user/models.py
@@ -12,33 +12,68 @@ class User:
         self.password = password
     
     def save(self):
-        mongo.db.users.insert({"UserName": self.name,
-                               "Password": bcrypt.generate_password_hash(self.password)})
+        #FIXME here may be a bug -> can a username duplicated?
+        # if the answer is No, here we should create a unique index with UserName
+        # if the answer is Yes, why to allow this...
+        mongo.db.users.insert(
+            {
+                "UserName": self.name,
+                "Password": bcrypt.generate_password_hash(self.password),
+            },
+            safe=True,
+        )
 
     @classmethod
     def search_by_name(cls, name):
-        return mongo.db.users.find(name).count()
+        '''
+            search users collection by UserName column
+            @name => target user
+        '''
+        return mongo.db.users.find_one({'UserName': name}) or {}
+
     
     @classmethod
     def check_user(cls, name, password):
-        user = mongo.db.users.find_one({'UserName': name})
+        '''
+            check the user by given (username, password)
+            @name => target user
+            @password => target user's password
+        '''
+        flag = False
+        user = mongo.db.users.find_one({'UserName': name}) or {}
+
+        if all([
+            user.get('UserName', "") == name,
+            bcrypt.check_password_hash(user.get("Password", ""), password),
+        ]):
+            flag = True
         
-        if user["UserName"] == name and \
-           bcrypt.check_password_hash(user["Password"], password):
-            return True
-        
-        return False
+        return flag
     
     @classmethod
     def get_user(cls, name, password):
-        return cls(name, password)
+        '''
+            get user info by given name, password
+            @name => target username
+            @password => target user's password
+        '''
+        return mongo.db.users.find_one({
+            'UserName': name,
+            'Password': bcrypt.generate_password_hash(password),
+        }) or {}
     
     @classmethod
     def get_user_by_id(cls, user_id):
-      return mongo.db.users.find_one({'_id': user_id})
-    
+        '''
+            get the user info by given user_id.
+            here the user_id is the ObjectId in python module <bson>
+            like:
+                from bson import ObjectId
+
+            @user_id => the given ObjectId which can identify the user
+        '''
+        return mongo.db.users.find_one({'_id': user_id}) or {}
+
     def get_user_id(self):
-        user = mongo.db.users.find_one({'UserName': self.name})
-        
-        return user['_id']
+        return mongo.db.users.find_one({'UserName': self.name}).get('_id')
 
