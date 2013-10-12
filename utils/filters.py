6ec28cdd8be8a0311fd3735df53234deb58571a5
titainium <knifewolf@gmail.com>
2013-10-12 11:52:04 +0800

    update the plan_update template file

--- utils/filters.py
+++ utils/filters.py
@@ -3,7 +3,10 @@
 
 from .const import PASSWORD_LENGTH
 
-__all__ = ['password']
+__all__ = ['password', 'to_string']
 
 def password(value):
-  return len(value) / PASSWORD_LENGTH * '*'
+    return len(value) / PASSWORD_LENGTH * '*'
+
+def to_string(val):
+    return ','.join(val)
