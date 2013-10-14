e753b1011608ecfe39a05ee446884346598fac2a
titainium <knifewolf@gmail.com>
2013-10-14 20:57:54 +0800

    update the plan_update and plan_add template file

--- utils/filters.py
+++ utils/filters.py
@@ -3,10 +3,13 @@
 
 from .const import PASSWORD_LENGTH
 
-__all__ = ['password', 'to_string']
+__all__ = ['password', 'to_string', 'get_nick_name']
 
 def password(value):
     return len(value) / PASSWORD_LENGTH * '*'
 
 def to_string(val):
     return ','.join(val)
+
+def get_nick_name(val):
+    return ','.join([usr['profile']['nick_name'] for usr in val])
