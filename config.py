32457f0c93194bfc814e50a9c603ff1160274cb5
TY@MacBook Pro <tianyu0915@gmail.com>
2013-10-20 12:37:29 +0800

    uploader

--- config.py
+++ config.py
@@ -5,4 +5,6 @@ class ConfigObj(object):
     BABEL_DEFAULT_LOCALE = 'en'
     DEBUG = True
     SECRET_KEY = 'FORM_SECRET'
-    
+    UPLOAD_FOLDER = '/tmp/prphoto/media' and DEBUG or '/var/prphoto/media'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
